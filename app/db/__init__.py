f676cb7f6cc5e539f3fd9a95261b80e42e36be52
DMarrs <diegomarrs@gmail.com>
2023-01-01 03:48:46 -0800

    added homepage views

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,9 +3,28 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db(app):
+    Base.metadata.create_all(engine)
+
+    # run this whenever a context is destroyed
+    app.teardown_appcontext(close_db)
+
+def get_db():
+    if 'db' not in g:
+        g.db = Session()
+
+    return Session()
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
